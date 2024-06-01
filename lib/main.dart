a96a01e96c7d922e7482044211da74c719ed1e9b
Afaq <afaqnaveed@gmail.com>
2024-06-02 01:51:12 +0500

    Everything is a widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "My App Bar",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),),
+          ],
+        ),
+        body: Center( // Wraped Container widget with Center which puts the Container widget in the center
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              //Curving the corners
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            //You can add another widget by adding child
+            child: Icon(
+              Icons.favorite,
+              color: Colors.white,
+              size: 64,
+            ),
+
+          ),
+        ), //This is the body component of the Scaffold, Inside body we have Container widget which is a flexible widget that is useful for many purposes
+      ),
+    );
   }
 }
